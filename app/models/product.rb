d32d48d7f514fc8932b6e6b0fef7b7b6570fb515
Wenhui Wang <w.eleventh@gmail.com>
2017-03-24 17:29:07 +0800

    add relationships among models

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,17 @@ class Product < ApplicationRecord
               with: /\.(gif|jpg|png)\Z/i,
               message: 'must be a URL for GIF, JPG or PNG image.'
             }
+
+  has_many :line_items
+
+  before_destroy :ensure_not_reference_by_any_line_item
+
+  private
+
+  def ensure_not_reference_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
