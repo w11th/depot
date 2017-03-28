9e1b159c719b205ea5fce41ff1400c35a8003217
Wenhui Wang <w.eleventh@gmail.com>
2017-03-28 18:17:55 +0800

    bug fix: fix redirect path after creating order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to @order, notice: 'Thank you for your order.' }
+        format.html { redirect_to stores_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
