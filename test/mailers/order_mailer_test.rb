0d2fff158264731690b4fabf545cbe8bc3d7b638
Wenhui Wang <w.eleventh@gmail.com>
2017-03-29 10:41:27 +0800

    mailer and integration test

---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match %r{1 x Rails, Angular, Postgres, and Bootstrap}, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match %r{<td>1&times;</td>\s*<td>Rails, Angular, Postgres, and Bootstrap</td>}, mail.body.encoded
+  end
+end
