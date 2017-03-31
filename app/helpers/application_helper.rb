c88a011361d08dba0b0d79295494ff39be2aa31e
Wenhui Wang <w.eleventh@gmail.com>
2017-03-31 15:45:28 +0800

    I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     attributes['style'] = "display: none" if condition
     content_tag('div', attributes, &block)
   end
+
+  CURRENCY_EXCHANGE_RATE = Hash.new(1)
+  CURRENCY_EXCHANGE_RATE['es'] = 1.3
+  def currency_to_locale(price)
+    price *= CURRENCY_EXCHANGE_RATE[I18n.locale.to_s]
+    number_to_currency price
+  end
 end
